44974533f1c9629c0072af9c0f957ad10aaa8ed4
Tom-ne <71755170+HeaG123@users.noreply.github.com>
2023-06-09 14:44:52 +0300

    made search easier to use

--- src/commands/search.rs
+++ src/commands/search.rs
@@ -1,9 +1,6 @@
 use serde_json::{Value, Error};
 
-use crate::{
-    input_helper::{flush_output_stream, get_user_input},
-    api::make_request,
-};
+use crate::lib::{input_helper::{flush_output_stream, get_user_input}, modrinth::{search_mod}};
 
 fn print_mod_info(json_str: &str) -> Result<(), Error> {
     let json: Value = serde_json::from_str(json_str)?;
@@ -47,21 +44,15 @@ pub(crate) async fn run() {
     print!("Enter mod to search for: ");
     flush_output_stream();
     let input = get_user_input();
-    let req = format!("https://api.modrinth.com/v2/search?query={}", input);
-    let headers = String::new();
 
-    match make_request(req, headers).await {
-        Ok(json) => {
-            if let Ok(pretty_json) = serde_json::to_string_pretty(&json) {
-                if let Err(err) = print_mod_info(&pretty_json) {
-                    eprintln!("Error: {:?}", err);
-                }
-            } else {
-                println!("Failed to format JSON");
+    match search_mod(&input).await {
+        Ok(pretty_json) => {
+            if let Err(err) = print_mod_info(&pretty_json) {
+                eprintln!("Error: {:?}", err);
             }
         },
         Err(err) => {
-            eprintln!("Error: {:?}", err)
+            eprintln!("Error: {:?}", err);
         }
     }
 }
