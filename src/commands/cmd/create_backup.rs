26968582f8ac6ad719338047457381484bd115bb
Tom-ne <71755170+HeaG123@users.noreply.github.com>
2023-07-23 19:55:42 +0300

    formatted create_backup

--- src/commands/cmd/create_backup.rs
+++ src/commands/cmd/create_backup.rs
@@ -1,8 +1,13 @@
-use std::{path::Path, io::{Read, Write}, fs::{self, File}, os::unix::prelude::PermissionsExt};
+use std::{
+    fs::{self, File},
+    io::{Read, Write},
+    os::unix::prelude::PermissionsExt,
+    path::Path,
+};
 
 use async_trait::async_trait;
-use chrono::{DateTime, Local, Timelike, Datelike};
-use zip::{ZipWriter, write::FileOptions, CompressionMethod};
+use chrono::{DateTime, Datelike, Local, Timelike};
+use zip::{write::FileOptions, CompressionMethod, ZipWriter};
 
 use crate::lib::modify::{command::Command, config_helper::read_config};
 
@@ -30,7 +35,10 @@ fn zip_folder_recursive(
     for entry in fs::read_dir(input_folder)? {
         let entry = entry?;
         let path = entry.path();
-        let name = path.strip_prefix(base_folder)?.to_string_lossy().into_owned();
+        let name = path
+            .strip_prefix(base_folder)?
+            .to_string_lossy()
+            .into_owned();
 
         if path.is_dir() {
             zip.add_directory(name, options)?;
@@ -62,7 +70,14 @@ impl Command for CreateBackupCommand {
         }
 
         let local_time: DateTime<Local> = Local::now();
-        let formatted_time = format!("{:02}-{:02}-{:02}-{:02}-{}", local_time.hour(), local_time.minute(), local_time.day(), local_time.month(), local_time.year());
+        let formatted_time = format!(
+            "{:02}-{:02}-{:02}-{:02}-{}",
+            local_time.hour(),
+            local_time.minute(),
+            local_time.day(),
+            local_time.month(),
+            local_time.year()
+        );
 
         let backup_folder_str = format!("{}/{}", mc_folder.display(), "backup");
         let backup_folder = Path::new(&backup_folder_str);
@@ -70,6 +85,7 @@ impl Command for CreateBackupCommand {
         let zip_file_path = format!("{}.zip", formatted_time);
         let output_zip = backup_folder.join(zip_file_path);
 
+        println!("Creating backup...");
         if let Err(e) = zip_folder(Path::new(&settings.mc_mod_dir), &output_zip) {
             println!("Failed to create backup: {:?}", e);
         } else {
